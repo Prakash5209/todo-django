cbf923fdff1abd0fd4ad7e2279ec3ad9ba1038d7
prakash chaudhary <prakashkchaudhary5209@gmail.com>
2023-06-10 16:12:32 +0545

    signup.html updated

--- task/models.py
+++ task/models.py
@@ -7,11 +7,16 @@ class TimeStampModel(models.Model):
     
     class Meta:
         ordering = ['-modified_at' ,'-created_at']
+        
+# class Status(models.TextChoices):
+#     NOT_FINISHED = 'incomplete','INCOMPLETED'
+#     FINISHED = 'completed','COMPLETED'
     
 class Task(TimeStampModel):
     user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,blank=True,null=True)
     title=models.CharField(max_length=100)
     description=models.TextField()
+    # status = models.CharField(max_length = 255,choices = Status.choices,default="Status.NOF_FINISHED")
     
     def __str__(self):
         return self.user
--- useraccount/forms.py
+++ useraccount/forms.py
@@ -9,8 +9,10 @@ class CustomUserCreationForm(UserCreationForm):
         
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
-        self.fields['email'].widget.attrs.update({'class':'form-control'})
-                
+        # self.fields['email'].widget.attrs.update({'class':'form-control'})
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({'class':'form-control'})
+                            
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model=CustomUserModel
